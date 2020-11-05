929f07437d35d362cb089def2ae3d2248c7de9e7
Azeddev28 <ahsan.zh28@gmail.com>
2020-11-06 03:07:13 +0500

    "patient history graph corrected"

--- src/app/patient-glucose-history/patient-glucose-history.component.ts
+++ src/app/patient-glucose-history/patient-glucose-history.component.ts
@@ -49,7 +49,7 @@ export class PatientGlucoseHistoryComponent implements OnInit {
       label: 'Patient Glucose' 
     },
   ];
-  glucoseReadingTime: Label[] = this.timesArray;
+  glucoseReadingTime: Label[] = ['1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm', '9 pm', '10 pm', '11 pm', '12 am', '1 am', '2 am', '3 am', '4 am', '5 am', '6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm'];
 
   ngOnInit(): void {
   }
